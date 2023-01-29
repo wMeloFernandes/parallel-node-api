8621edbb1fbf9c4686c4a7ab52bf4225c5ad753d
Will Melo <w.melo.fernandes.un@gmail.com>
2023-01-29 20:52:15 -0300

    add basic server structure

---
+++ src/config/server-config.ts
@@ -0,0 +1,53 @@
+import { Server } from '@overnightjs/core'
+import { json, urlencoded } from 'body-parser'
+import helmet from 'helmet'
+
+export class SetupServer extends Server {
+
+	constructor (private port = process.env.PORT || 8000) {
+		super()
+	}
+
+	public init (): void {
+		this.setupExpress()
+		this.setupMiddlewares()
+		this.setupControllers()
+	}
+
+	// Basic Express Setup
+	private setupExpress (): void {
+		this.app.use(json({ limit: '10mb' }))
+		this.app.use(urlencoded({ extended: true }))
+
+		// Setting Headers
+		this.app.use((req, res, next) => {
+			res.setHeader('Access-Control-Allow-Origin', '*')
+			res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+			res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE')
+			res.setHeader('Content-Type', 'application/json')
+			next()
+		})
+	}
+
+	// Setting static directories, protection headers and CORS
+	private setupMiddlewares (): void {
+		this.app.use(helmet())
+	}
+
+	// Setup of controllers to enabled routes associated to it
+	private setupControllers (): void {
+		this.addControllers([])
+	}
+
+	// Starting Server
+	public start (): void {
+		// Starting server
+		const server = this.app.listen(this.port, () => {
+			console.log('Starting Server...')
+		})
+
+		process.on('SIGTERM', () => {
+			server.close(() => { })
+		})
+	}
+}
--- src/main/app.ts
+++ src/main/app.ts
@@ -1,2 +1,6 @@
-console.log('')
-console.log('111')
+import 'reflect-metadata'
+import { SetupServer } from '../config/server-config'
+
+const app = new SetupServer()
+app.init()
+app.start()
