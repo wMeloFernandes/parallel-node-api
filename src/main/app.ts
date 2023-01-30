710b156d300e9bc0ea89b495e6d33f3ce1072e30
Will Melo <w.melo.fernandes.un@gmail.com>
2023-01-30 11:13:32 -0300

    update docker-compose file

--- src/main/app.ts
+++ src/main/app.ts
@@ -3,16 +3,17 @@ import * as dotenv from 'dotenv'
 import 'reflect-metadata'
 import { SetupServer } from '../config/server-config'
 dotenv.config()
+
 mongoose.set("strictQuery", false);
 mongoose
-      .connect(process.env.MONGO_URI, {
+      .connect(`${process.env.MONGO_URI}/${process.env.DB_DATABASE}`, {
         useNewUrlParser: true,
-        useUnifiedTopology: true,
+        useUnifiedTopology: true
       } as ConnectOptions)
-      .then((res) => {
+      .then(async (res) => {
         const app = new SetupServer()
 				app.init()
-				app.start()
+        app.start()
       })
       .catch((err) => {
         console.log(err);
