14a72c584f18d3728a44efdac759c140ad1377b2
Will Melo <w.melo.fernandes.un@gmail.com>
2023-01-29 22:15:32 -0300

    start server checking if mongo connections is up

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,6 +1,19 @@
+import mongoose, { ConnectOptions } from "mongoose";
+import * as dotenv from 'dotenv'
 import 'reflect-metadata'
 import { SetupServer } from '../config/server-config'
-
-const app = new SetupServer()
-app.init()
-app.start()
+dotenv.config()
+mongoose.set("strictQuery", false);
+mongoose
+      .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      } as ConnectOptions)
+      .then((res) => {
+        const app = new SetupServer()
+				app.init()
+				app.start()
+      })
+      .catch((err) => {
+        console.log(err);
+      });
